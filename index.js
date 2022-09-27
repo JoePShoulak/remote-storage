e69eefe2b9837ba3671267f0cb860a80e78d401b
Joe P Shoulak <joe@squarecompass.io>
2022-09-27 01:11:45 -0500

    Adding routes, added dummy data

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import taskRoutes from "./routes/tasks.js";
+
 const app = express();
 const port = 5000;
 
 app.use(bodyParser.json());
+
+app.use("/tasks", taskRoutes);
 
 app.get("/", (req, res) => {
     console.log("[TEST]!");
--- routes/tasks.js
+++ routes/tasks.js
@@ -2,4 +2,29 @@ import express from "express";
 
 const router = express.Router();
 
+var taskList = [{
+        id: 1,
+        title: "Testing",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        dueDate: "2022-09-27",
 
+        // Bools
+        weather: true,
+        completed: false,
+    }, {
+        id: 2,
+        title: "Testing 2",
+        description: "Wassup Wassup Wassup Wassup Wassup.",
+        dueDate: "2022-09-27",
+
+        // Bools
+        weather: false,
+        completed: true,
+    }
+];
+
+router.get("/", (req, res) => {
+    res.send(taskList);
+});
+
+export default router;
