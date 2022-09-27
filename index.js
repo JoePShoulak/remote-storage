5224b2be650ce61088087a83b3fbc76c1d875f13
Joe P Shoulak <joe@squarecompass.io>
2022-09-27 01:51:41 -0500

    Added CORS support

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
-
+import cors from "cors";
 import taskRoutes from "./routes/tasks.js";
 
 const app = express();
 const port = 5000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/tasks", taskRoutes);
 
