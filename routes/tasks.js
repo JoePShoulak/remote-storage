ad92445f3cf628a34584982496269bf97b13c219
Joe P Shoulak <joe@squarecompass.io>
2022-09-27 01:35:55 -0500

    basic POST route implemeneted

--- routes/tasks.js
+++ routes/tasks.js
@@ -7,8 +7,6 @@ var taskList = [{
         title: "Testing",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
         dueDate: "2022-09-27",
-
-        // Bools
         weather: true,
         completed: false,
     }, {
@@ -16,15 +14,16 @@ var taskList = [{
         title: "Testing 2",
         description: "Wassup Wassup Wassup Wassup Wassup.",
         dueDate: "2022-09-27",
-
-        // Bools
         weather: false,
         completed: true,
     }
 ];
 
-router.get("/", (req, res) => {
-    res.send(taskList);
-});
+router.get("/", (req, res) => res.send(taskList) );
+
+router.post("/", (req, res) => {
+    taskList = req.body;
+    res.send("");
+})
 
 export default router;
